8743f717241ca0a969f897fd100c8634e0df66b6
Chris Klosowski <cklosowski@gmail.com>
2017-05-05 16:30:04 -0700

    Version bump and remove readme.txt

--- easy-digital-downloads-duplicate-downloads.php
+++ easy-digital-downloads-duplicate-downloads.php
@@ -3,7 +3,7 @@
 Plugin Name: Easy Digital Downloads - Duplicate Downloads
 Plugin URI: https://easydigitaldownloads.com/downloads/duplicate-download/
 Description: Duplicates EDD Downloads
-Version: 1.0
+Version: 1.0.1
 Author: Easy Digital Downloads
 Author URI: https://easydigitaldownloads.com
 Text Domain: duplicate_downloads
@@ -17,7 +17,7 @@ function edd_dd_wp() {
 			require_once( dirname( __FILE__ ) . '/admin.php' );
 		}
 
-		$license = new EDD_License( __FILE__, 'Duplicate Downloads', '1.0', 'EDD Team' );
+		$license = new EDD_License( __FILE__, 'Duplicate Downloads', '1.0.1', 'EDD Team' );
 	}
 }
 add_action('plugins_loaded','edd_dd_wp');
