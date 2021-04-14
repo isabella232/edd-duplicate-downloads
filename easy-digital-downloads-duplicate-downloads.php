6d17cf5983a87d9b9cd88543bee3ac0676f77c9a
LisaCee <lisacanini@sandhillsdev.com>
2021-04-14 15:58:41 -0700

    #9 Update author, add item_id to license

--- easy-digital-downloads-duplicate-downloads.php
+++ easy-digital-downloads-duplicate-downloads.php
@@ -4,8 +4,8 @@ Plugin Name: Easy Digital Downloads - Duplicate Downloads
 Plugin URI: https://easydigitaldownloads.com/downloads/duplicate-download/
 Description: Duplicates EDD Downloads
 Version: 1.0.1
-Author: Easy Digital Downloads
-Author URI: https://easydigitaldownloads.com
+Author: Sandhills Development, LLC
+Author URI: https://sandhillsdev.com
 Text Domain: duplicate_downloads
  */
 
@@ -17,7 +17,7 @@ function edd_dd_wp() {
 			require_once( dirname( __FILE__ ) . '/admin.php' );
 		}
 
-		$license = new EDD_License( __FILE__, 'Duplicate Downloads', '1.0.1', 'EDD Team' );
+		$license = new EDD_License( __FILE__, 'Duplicate Downloads', '1.0.1', 'Sandhills Development, LLC', null, null, null, 74950 );
 	}
 }
-add_action('plugins_loaded','edd_dd_wp');
+add_action( 'plugins_loaded', 'edd_dd_wp' );
